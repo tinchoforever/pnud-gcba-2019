5a9be55e777c7e3f9ad2d4b8b5769c53aefb8022
andres Snitcofsky <andres@soviet.com.ar>
2019-12-05 20:39:14 -0300

    sin steps para el primer grafico

--- scripts/ba-svg-map.js
+++ scripts/ba-svg-map.js
@@ -2,43 +2,16 @@
 
 
 function transicionaMapa(step) {
-  switch (step) {
-    case 0: // esto es cuando estamos en el inicio
-     
-    break;
-    
-    case 1: // esto es el mapa con los lugares 
+
       d3.selectAll("#svgFamiliasIcons,#svgFamiliasExtra").transition().duration(500).style("opacity", d3.select("#svgFamiliasIcons").style("opacity") == 1 ? 0 : 1);
       d3.select("#svgPersonas").transition().duration(500)
         .attr("transform", d3.select("#svgFamiliasIcons").style("opacity") == 1 ? "translate(0,-240)" : "translate(0,0)");
 
-    break;
-
-    case 2: // esto es el mapa con los lugares al aire libre
-      d3.selectAll("#svgPersonasIcons,#svgPersonasExtra").transition().duration(500).style("opacity", d3.select("#svgPersonasIcons").style("opacity") == 1 ? 0 : 1);
+      d3.selectAll("#svgPersonasIcons,#svgPersonasExtra").transition().duration(500).delay(1500).style("opacity", d3.select("#svgPersonasIcons").style("opacity") == 1 ? 0 : 1);
 
      
-    break;
-
-
-     case 3:
-    
-
-      break;
-
-
-
-      case 4: 
-    
-      break;
-     
-      case 5: 
-    
-
-      break;
-      
   
-  }  
+    
   
 
 }
